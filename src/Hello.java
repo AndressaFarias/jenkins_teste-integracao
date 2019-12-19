d34e087f4b4ff22209578a72b50589e85eaebf42
Andressa Farias <andressa.farias@compasso.com.br>
2019-12-19 11:33:49 -0200

    master

--- src/Hello.java
+++ src/Hello.java
@@ -2,12 +2,9 @@
 public class Hello {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-=======
 		System.out.println("Teste de Integra��o com o Jenkins");
 		System.out.println("Hello DEV");
 		System.out.println("Hello Homolog");
 
->>>>>>> homolog
 	}
 }
